b7eab72fc70d38670f5191b1fd98a252292cc6c7
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2013-12-08 13:39:43 +0100

    Implement 'prepare-deps' meta command
    'rebar prepare-deps' is equivalent to 'rebar -r get-deps compile'.

--- src/rebar.erl
+++ src/rebar.erl
@@ -362,6 +362,8 @@ list-templates                           List available templates
 
 doc                                      Generate Erlang program documentation
 
+prepare-deps                             Run 'rebar -r get-deps compile'
+
 check-deps                               Display to be fetched dependencies
 get-deps                                 Fetch dependencies
 update-deps                              Update fetched dependencies
@@ -474,6 +476,7 @@ command_names() ->
      "help",
      "list-deps",
      "list-templates",
+     "prepare-deps",
      "qc",
      "update-deps",
      "overlay",
